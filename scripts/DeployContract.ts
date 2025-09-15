2ba684f67d0e257541c5fe6c36bcdd40c0cf0be8
Corey <corey@dolomite.io>
2025-09-15 14:21:24 -0400

    updated deployment

--- scripts/DeployContract.ts
+++ scripts/DeployContract.ts
@@ -1,12 +1,13 @@
 import { execSync } from 'child_process';
 import { promisify } from 'es6-promisify';
 import fs from 'fs';
-import LiquidatorProxyV4WithGenericTrader from '../build/contracts/LiquidatorProxyV4WithGenericTrader.json';
+import Web3 from 'web3';
+import ICREATE3Factory from '../build/contracts/ICREATE3Factory.json';
+import MultiCall from '../build/contracts/MultiCall.json';
 import deployed from '../migrations/deployed.json';
 import { ConfirmationType, DolomiteMargin } from '../src';
 
 const truffle = require('../truffle.js');
-const { getChainId } = require('../migrations/helpers.js');
 
 const writeFileAsync = promisify(fs.writeFile);
 
@@ -25,34 +26,44 @@ async function deploy(): Promise<void> {
     return Promise.reject(new Error('Incorrect node version! Expected v20.17.0'));
   }
 
-  const contractName = LiquidatorProxyV4WithGenericTrader.contractName;
+  const contractName = MultiCall.contractName;
   const networkId = truffle.networks[network]['network_id'];
   const provider = truffle.networks[network].provider();
   const dolomiteMargin = new DolomiteMargin(provider, networkId);
   const deployer = (await dolomiteMargin.web3.eth.getAccounts())[0];
   console.log('Deploying from: ', deployer);
 
-  const contract = new dolomiteMargin.web3.eth.Contract(LiquidatorProxyV4WithGenericTrader.abi);
-  const chainId = getChainId(network);
-  const txResult = await dolomiteMargin.contracts.callContractFunction(
-    contract.deploy({
-      data: LiquidatorProxyV4WithGenericTrader.bytecode,
-      arguments: [
-        chainId,
-        deployed.Expiry[chainId].address,
-        deployed.DolomiteMargin[chainId].address,
-        deployed.LiquidatorAssetRegistry[chainId].address,
-      ],
-    }),
-    { confirmationType: ConfirmationType.Confirmed, from: deployer },
+  const code = new dolomiteMargin.web3.eth.Contract(MultiCall.abi)
+    .deploy({
+      data: MultiCall.bytecode,
+      arguments: [],
+    })
+    .encodeABI();
+  const salt = Web3.utils.keccak256(dolomiteMargin.web3.eth.abi.encodeParameters(['string'], [contractName]));
+
+  const factory = new dolomiteMargin.web3.eth.Contract(
+    ICREATE3Factory.abi,
+    '0xa8F7e7A361De6A2172fcb2accE68bd21597599F7',
+  );
+  const contractAddress = await dolomiteMargin.contracts.callConstantContractFunction(
+    factory.methods.getDeployed(deployer, salt)
   );
 
-  console.log(`Deployed ${contractName} to ${txResult.contractAddress}`);
+  let txHash = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  if ((await dolomiteMargin.web3.eth.getCode(contractAddress)) === '0x') {
+    const txResult = await dolomiteMargin.contracts.callContractFunction(factory.methods.deploy(salt, code), {
+      confirmationType: ConfirmationType.Confirmed,
+      from: deployer,
+    });
+    txHash = txResult.transactionHash;
+  }
+
+  console.log(`Deployed ${contractName} to ${contractAddress}`);
   // sleeping for 5 seconds to allow for the transaction to settle before verification
   console.log('Sleeping for 5 seconds...');
   await sleep(5000);
 
-  execSync(`truffle run verify --network ${network} ${contractName}@${txResult.contractAddress}`, {
+  execSync(`truffle run verify --network ${network} ${contractName}@${contractAddress}`, {
     stdio: 'inherit',
   });
 
@@ -60,8 +71,8 @@ async function deploy(): Promise<void> {
 
   deployed[contractName][networkId] = {
     links: {},
-    address: txResult.contractAddress,
-    transactionHash: txResult.transactionHash,
+    address: contractAddress,
+    transactionHash: txHash,
   };
 
   const json = JSON.stringify(deployed, null, 4).concat('\n');
